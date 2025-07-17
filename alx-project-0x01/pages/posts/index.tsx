a91f2db0c34f216acd564ffe25a6a7e4dd9fb2e3
benchzak <benchzakaria3@gmail.com>
2025-07-17 20:03:50 +0100

    task 0:  setup projet 1

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PostsPage: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold mb-4">Posts</h1>
+      {/* Posts will be listed here */}
+    </div>
+  );
+};
+
+export default PostsPage;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UsersPage: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold mb-4">Users</h1>
+      {/* Users will be listed here */}
+    </div>
+  );
+};
+
+export default UsersPage;
